e34e36c607f9c4da38314da7b854c9792cd0399d
philipgriffin <philipgriffin88@gmail.com>
2016-05-24 23:33:24 +0100

    ignored .idea, added concat, separated directive into separate file

---
+++ assets/js/smooth-scroll-directive.js
@@ -0,0 +1,14 @@
+angular.module('personalWebsite').directive('animateScroll', function(){
+    return {
+        restrict: 'A',
+        scope: {
+            scrollTo: '@'
+        },
+        link: function(scope, $elm, attr){
+            $elm.on('click', function(){
+                console.log('click');
+                $('html,body').animate({scrollTop: $('#' + scope.scrollTo).offset().top - 40 +'px'}, 450);
+            });
+        }
+    };
+});
--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass         = require('gulp-sass');
 var watch        = require('gulp-watch');
 var uglify       = require('gulp-uglify');
 var browserSync  = require('browser-sync');
+var concat       = require('gulp-concat');
 
 gulp.task('sass', function () {
   return gulp.src('assets/css/main.sass')
@@ -11,11 +12,17 @@ gulp.task('sass', function () {
     .pipe(browserSync.stream());
 });
 
-gulp.task('js', function() {
+gulp.task('minjs', function() {
   return gulp.src('assets/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('assets/js/min'))
+    .pipe(gulp.dest('assets/js/min/'))
     .pipe(browserSync.stream());
+});
+
+gulp.task('concatjs', ['minjs'], function() {
+  return gulp.src('assets/js/min/*.js')
+      .pipe(concat('allJs.min.js'))
+      .pipe(gulp.dest('assets/js/concat/'));
 });
 
 gulp.task('html', function(){
@@ -35,3 +42,6 @@ gulp.task('browserSync', function() {
 });
 
 gulp.task('default', ['browserSync']);
+
+
+gulp.task('js', ['minjs', 'concatjs']);
